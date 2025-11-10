466b0dcda05f3fd1013f399159e9dca4a861cb2e
LuanRibeiroGit <luancoxa2009@gmail.com>
2025-11-10 17:06:42 -0300

    feat(account): criação do serviço findOne

--- src/modules/accounts/account.service.ts
+++ src/modules/accounts/account.service.ts
@@ -4,5 +4,11 @@ import { HttpExceptionFilter } from '../../common/filters/http-exception.filter'
 const prisma = new PrismaClient()
 
 export class AccountService {
-    
+    async findOne (accounts: number){
+        const account = await prisma.account.findUnique({
+            where: { number: accounts },
+        })
+        if (!account)throw new HttpExceptionFilter('Conta não encontrada', 404)
+        return account
+    }
 }
