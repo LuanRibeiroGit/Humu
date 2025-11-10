48d696eb9756112233bbfa41394f888f55e3cb7a
LuanRibeiroGit <luancoxa2009@gmail.com>
2025-11-10 17:06:24 -0300

    feat(account): criação do service

---
+++ src/modules/accounts/account.service.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from "@prisma/client"
+import { HttpExceptionFilter } from '../../common/filters/http-exception.filter'
+
+const prisma = new PrismaClient()
+
+export class AccountService {
+    
+}
